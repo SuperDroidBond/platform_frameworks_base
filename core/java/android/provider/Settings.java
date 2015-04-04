254484c632be425b5323aae0f50f52f98f6ae68d
Dave Kessler <activethrasher00@gmail.com>
2015-04-04 15:47:24 +0200

    Add membar to recents options [1/2]
    https://github.com/BenzoRoms/frameworks_base/commit/98de755cb5cb56b1778fd7fd38a9d36e90b2ade5
    https://github.com/BenzoRoms/frameworks_base/commit/0a454d19e453b7294c6df54cbb80b217405a2798
    
    thanks to varund7726 for the spacing
    
    Change-Id: Id1e799729543e385767ec8b68b126a20c4874baf
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3948,7 +3948,7 @@ public final class Settings {
         /** @hide */
         private static final Validator SHOW_BATTERY_PERCENT_VALIDATOR = sBooleanValidator;
 
-        /** Beginning of ABC custom setttings**/
+        /** Beginning of ColtOS custom setttings**/
 
         /**
          * Use latest screen-on brightness value for Ambient Display
@@ -3961,6 +3961,12 @@ public final class Settings {
          * @hide
          */
         public static final String LAST_DOZE_AUTO_BRIGHTNESS = "last_doze_auto_brightness";
+
+	/**
+         * show the membar in recents
+         *  @hide
+         */
+        public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
 
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
