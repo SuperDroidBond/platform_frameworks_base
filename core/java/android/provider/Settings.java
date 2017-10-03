182ab1e15dc25b828ba49e5916a6a76e0f568a69
maxwen <max.weninger@gmail.com>
2017-10-03 14:32:57 +0200

    [1/2] base: Battery style tweaks
    for now just add hide/show image and or percentage
    
    Change-Id: I2d781be39f1f99c28166b63ea40d783a4316ef93

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4373,6 +4373,13 @@ public final class Settings {
         public static final String RECENTS_ICON_PACK = "recents_icon_pack";
 
         /**
+         * Enable/Disable battery icon
+         * String.
+         * @hide
+         */
+        public static final String SHOW_BATTERY_IMAGE = "status_bar_show_battery_image";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4565,6 +4572,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(PROXIMITY_AUTO_SPEAKER);
             PRIVATE_SETTINGS.add(PROXIMITY_AUTO_SPEAKER_DELAY);
             PRIVATE_SETTINGS.add(PROXIMITY_AUTO_SPEAKER_INCALL_ONLY);
+            PRIVATE_SETTINGS.add(SHOW_BATTERY_IMAGE);
         }
 
         /**
