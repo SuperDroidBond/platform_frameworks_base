Colt-Oreo/android_frameworks_base-1

Add Colt tap Platlogo
Thanks to xX-Yoshi-Xx & Asher Simonds

Change-Id: I35ce9b8dc2cfa1135207e904faf8f3bc9e1d57dc
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

core/java/android/os/Build.java
@@ -48,6 +48,11 @@ public class Build {
     /** A build ID string meant for displaying to the user */
     public static final String DISPLAY = getString("ro.build.display.id");
 
+    /**
+     * @hide
+     */
+    public static final String COLT_DISPLAY_VERSION = getString("ro.colt.display.version");
+
     /** The name of the overall product. */
     public static final String PRODUCT = getString("ro.product.name");
 

SuperDroidBond/platform_frameworks_base

core/java/android/os/Build.java
@@ -44,6 +44,11 @@ public class Build {
     /** A build ID string meant for displaying to the user */
     public static final String DISPLAY = getString("ro.build.display.id");
 
+    /**
+     * @hide
+     */
+    public static final String COLT_VERSION = getString("ro.colt.display.version");
+
     /** The name of the overall product. */
     public static final String PRODUCT = getString("ro.product.name");
 