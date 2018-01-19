a9fb6cd4058febaa74e70b34575138b687387e7d
ezio84 <brabus84@gmail.com>
2018-01-19 15:30:10 +0100

    Ambient music: let hide clock and show track info in center screen [1/2]
    when a new track is played, allow to show track infos only,
    without the clock and notifications icons.
    In this case, also move track info to the screen center.
    
    This doesn't affect normal system ambient notifications, they'll keep
    clock and other stuff.
    Change-Id: I4b249464f17a1742f8926a48d63a82b11e3bc075

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -60,6 +60,7 @@ import android.widget.TextView;
 import com.android.internal.util.ArrayUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.systemui.ChargingView;
+import com.android.systemui.doze.DozeLog;
 import com.android.systemui.statusbar.policy.DateView;
 
 import java.util.Locale;
@@ -116,6 +117,7 @@ public class KeyguardStatusView extends GridLayout implements
     private boolean mShowDate;
     private int mClockSelection;
     private int mDateSelection;
+    private boolean mForcedMediaDoze;
 
     private KeyguardUpdateMonitorCallback mInfoCallback = new KeyguardUpdateMonitorCallback() {
 
@@ -673,7 +675,7 @@ public class KeyguardStatusView extends GridLayout implements
         final int N = mClockContainer.getChildCount();
         for (int i = 0; i < N; i++) {
             View child = mClockContainer.getChildAt(i);
-            if (ArrayUtils.contains(mVisibleInDoze, child)) {
+            if (!mForcedMediaDoze && ArrayUtils.contains(mVisibleInDoze, child)) {
                 continue;
             }
             child.setAlpha(dark ? 0 : 1);
@@ -695,12 +697,21 @@ public class KeyguardStatusView extends GridLayout implements
 
     public void setPulsing(boolean pulsing) {
         mPulsing = pulsing;
+    }
+
+    public void setForcedMediaPulse(int reason) {
+        mForcedMediaDoze =
+                reason == DozeLog.PULSE_REASON_FORCED_MEDIA_NOTIFICATION;
         updateDozeVisibleViews();
     }
 
     private void updateDozeVisibleViews() {
         for (View child : mVisibleInDoze) {
-            child.setAlpha(mDarkAmount == 1 && mPulsing ? 0.8f : 1);
+            if (!mForcedMediaDoze) {
+                child.setAlpha(mDarkAmount == 1 && mPulsing ? 0.8f : 1);
+            } else {
+                child.setAlpha(mDarkAmount == 1 ? 0 : 1);
+            }
         }
     }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -543,7 +543,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     // Full Screen Gestures
     protected ScreenGesturesController gesturesController;
-    private boolean mAmbientMediaPlaying;
+    private int mAmbientMediaPlaying;
 
     // Tracking finger for opening/closing.
     boolean mTracking;
@@ -742,7 +742,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     }
 
     public void triggerAmbientForMedia() {
-        if (mAmbientMediaPlaying) {
+        if (mAmbientMediaPlaying != 0) {
             mDozeServiceHost.fireNotificationMedia();
         }
     }
@@ -6250,12 +6250,14 @@ public class StatusBar extends SystemUI implements DemoMode,
                         // Otherwise just show the always-on screen.
                         setPulsing(pulsingEntries);
                     }
+                    setForcedMediaPulse(mAmbientMediaPlaying == 2 ? reason : -1);
                 }
 
                 @Override
                 public void onPulseFinished() {
                     callback.onPulseFinished();
                     setPulsing(null);
+                    setForcedMediaPulse(-1);
                 }
 
                 private void setPulsing(Collection<HeadsUpManager.HeadsUpEntry> pulsing) {
@@ -6263,6 +6265,14 @@ public class StatusBar extends SystemUI implements DemoMode,
                     mNotificationPanel.setPulsing(pulsing != null);
                     mVisualStabilityManager.setPulsing(pulsing != null);
                     mIgnoreTouchWhilePulsing = false;
+                }
+
+                private void setForcedMediaPulse(int reason) {
+                    mNotificationPanel.setForcedMediaPulse(reason);
+                    mNotificationShelf.setForcedMediaPulse(reason);
+                    if (mAmbientIndicationContainer != null) {
+                        ((AmbientIndicationContainer)mAmbientIndicationContainer).setForcedMediaPulse(reason);
+                    }
                 }
             }, reason);
         }
@@ -6880,7 +6890,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     private void setForceAmbient() {
         mAmbientMediaPlaying = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.FORCE_AMBIENT_FOR_MEDIA, 0,
-                UserHandle.USER_CURRENT) == 1;
+                UserHandle.USER_CURRENT);
     }
 
     protected final ContentObserver mNavbarObserver = new ContentObserver(mHandler) {
