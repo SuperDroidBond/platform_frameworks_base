a4ca061b5151a2584144d0fcd320965f28f07487
ezio84 <brabus84@gmail.com>
2018-04-10 11:49:46 +0200

    Statusbar left clock: add start padding
    and make it care about the right battery icon end padding
    to kill people ocd
    
    Change-Id: I99523ea340e3d93e10521a8b8178342e5a962cc4
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -6595,6 +6595,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     }
 
+    // Called from CollapsedStatusBarFragment observer
     public void updateBatterySettings() {
         if (mStatusBarView != null) {
             mStatusBarView.updateBatterySettings();
