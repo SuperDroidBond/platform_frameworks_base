ee8e95050733df38081f8ed32165821b04920a1a
ezio84 <brabus84@gmail.com>
2018-04-10 11:49:46 +0200

    Statusbar left clock: add start padding
    and make it care about the right battery icon end padding
    to kill people ocd
    
    Change-Id: I99523ea340e3d93e10521a8b8178342e5a962cc4
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -127,6 +127,13 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             mContentResolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUSBAR_CLOCK_DATE_FORMAT),
                     false, this, UserHandle.USER_ALL);
+            mContentResolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.STATUS_BAR_BATTERY_STYLE),
+                    false, this, UserHandle.USER_ALL);
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SHOW_BATTERY_PERCENT),
+                    false, this, UserHandle.USER_ALL);
+
         }
 
         @Override
@@ -136,6 +143,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             ((Clock)mCenterClock).updateSettings();
             mStatusBarComponent.updateBatterySettings();
             mStatusBarComponent.updateQsbhClock();
+            mStatusBarComponent.updateBatterySettings();
         }
     }
 
