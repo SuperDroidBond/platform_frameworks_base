da779ac20c2f271ffe5881bd5b60cae8cdd323c7
Dragon-oniv <nvrevrgivup@gmail.com>
2018-04-17 17:54:57 +0000

    Move VoLTE switch to ColtInteriors
    Signed-off-by: Dragon-oniv <nvrevrgivup@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4717,6 +4717,12 @@ public final class Settings {
         public static final String IMMERSIVE_RECENTS = "immersive_recents";
 
 
+       /**
+ 	 * Whether to display VoLTE icon when VoLTE present
+         * @hide
+         */
+        public static final String STATUS_BAR_VOLTE = "status_bar_volte";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
