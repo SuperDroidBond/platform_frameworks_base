f7bdc8f4bdfe92d4f2a41d9f97f204a283df8ab9
Pranav Vashi <neobuddy89@gmail.com>
2018-04-20 00:54:15 +0530

    Fix battery percentage disappearing in portrait mode
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
+++ packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
@@ -507,7 +507,7 @@ public class BatteryMeterDrawableBase extends Drawable {
         boolean pctOpaque = false;
         float pctX = 0, pctY = 0;
         String pctText = null;
-        if (!mCharging && !mPowerSaveEnabled && level > mCriticalLevel && mShowPercent) {
+        if (!mCharging && !mPowerSaveEnabled && mShowPercent) {
             mTextPaint.setColor(getColorForLevel(level));
             final float full = horizontal ? 0.60f : 0.38f;
             final float nofull = horizontal ? 0.75f : 0.5f;
