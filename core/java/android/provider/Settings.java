2875d36a3aed0629574b682531d21133a2e0e5ee
RakeshBatra <raakesh.batra@rediffmail.com>
2018-05-18 01:15:24 +0000

    fix for ls charging info

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4362,6 +4362,12 @@ public final class Settings {
          */
         public static final String QS_COLUMNS_LANDSCAPE = "qs_columns_landscape";
 
+	/**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
         /**
          * Whether to launch default music player when headset plugged in
          * 0 = don't do anything (default)
