4d07a1e3b419ecd781b44bc7c6815fe9d861de62
RakeshBatra <raakesh.batra@rediffmail.com>
2018-05-30 03:35:46 +0000

    Fix compilation
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenrecordTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenrecordTile.java
@@ -44,7 +44,7 @@ public class ScreenrecordTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.EXTENSIONS;
+        return MetricsEvent.COLT;
     }
 
     @Override
