AOKP/frameworks_base

Fw_base - custom Toast color options (1/2)
- Custom toast text color 2/2
https://github.com/CyanideL/android_frameworks_base/commit/5768bb556282235dc883571fdd649f4f086c3d6b

- Toast icon color 2/2
https://github.com/CyanideL/android_frameworks_base/commit/580fa613d9bc94b8388e1edc06da1ffc2b106820

AICP'fied:
       - Cleaned up and adapted RandomColorHelper

Change-Id: Ie1e1e3d506dce9c5f15d8569dd474596cefba866
Signed-off-by: Varun Date <date.varun123@gmail.com>

core/java/com/android/internal/util/aokp/RandomColorHelper.java
@@ -1,0 +1,43 @@
+/*
+* Copyright (C) 2016 Cyanide Android (rogersb11)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.aokp;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.provider.Settings;
+
+public class RandomColorHelper {
+
+    public static ColorStateList getToastIconColorList(Context context) {
+        return ColorStateList.valueOf(getToastIconColor(context));
+    }
+
+    public static int getToastIconColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.TOAST_ICON_COLOR, Color.WHITE);
+    }
+
+    public static ColorStateList getToastTextColorList(Context context) {
+        return ColorStateList.valueOf(getToastTextColor(context));
+    }
+
+    public static int getToastTextColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.TOAST_TEXT_COLOR, Color.WHITE);
+    }
+}

SuperDroidBond/platform_frameworks_base

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

core/java/com/android/internal/util/colt/RandomColorHelper.java
@@ -1,0 +1,43 @@
+/*
+* Copyright (C) 2016 Cyanide Android (rogersb11)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.colt;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.provider.Settings;
+
+public class RandomColorHelper {
+
+    public static ColorStateList getToastIconColorList(Context context) {
+        return ColorStateList.valueOf(getToastIconColor(context));
+    }
+
+    public static int getToastIconColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.TOAST_ICON_COLOR, Color.WHITE);
+    }
+
+    public static ColorStateList getToastTextColorList(Context context) {
+        return ColorStateList.valueOf(getToastTextColor(context));
+    }
+
+    public static int getToastTextColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.TOAST_TEXT_COLOR, Color.BLACK);
+    }
+}