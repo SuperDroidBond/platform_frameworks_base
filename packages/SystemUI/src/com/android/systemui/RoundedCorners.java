BootleggersROM/frameworks_base

base: Fix header images padding on content padding
This is more a workaround than an actual fix and it's for the header images from omnistyle.
This make it ignore the padding made with rounded_corner_content_padding on the main QS and
it applies to where the most visible change is done: On the QS icon part. It may break padding
on other places of qs, didn't saw anything of that.

This commit removes the padding of this example:
https://i.imgur.com/YzZtpxv.jpg

packages/SystemUI/src/com/android/systemui/RoundedCorners.java
@@ -143,7 +143,7 @@ public class RoundedCorners extends SystemUI implements Tunable {
             fragmentHostManager.addTagListener(CollapsedStatusBarFragment.TAG,
                     new TunablePaddingTagListener(padding, R.id.status_bar));
             fragmentHostManager.addTagListener(QS.TAG,
-                    new TunablePaddingTagListener(padding, R.id.header));
+                    new TunablePaddingTagListener(padding, R.id.headerIcons));
         }
     }
 
@@ -198,7 +198,12 @@ public class RoundedCorners extends SystemUI implements Tunable {
         private TunablePadding mTunablePadding;
 
         public TunablePaddingTagListener(int padding, int id) {
-            mPadding = padding;
+            
+            if (id == R.id.headerIcons) {
+                mPadding = padding + 8;
+            } else {
+                mPadding = padding;
+            } 
             mId = id;
         }
 

SuperDroidBond/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/RoundedCorners.java
@@ -153,7 +153,7 @@ public class RoundedCorners extends SystemUI implements Tunable {
             }
             if (qsPadding != 0) {
                 fragmentHostManager.addTagListener(QS.TAG,
-                        new TunablePaddingTagListener(qsPadding, R.id.header));
+                        new TunablePaddingTagListener(qsPadding, R.id.headerIcons));
             }
         }
     }
@@ -209,7 +209,12 @@ public class RoundedCorners extends SystemUI implements Tunable {
         private TunablePadding mTunablePadding;
 
         public TunablePaddingTagListener(int padding, int id) {
-            mPadding = padding;
+
+            if (id == R.id.headerIcons) {
+                mPadding = padding + 8;
+            } else {
+                mPadding = padding;
+            }
             mId = id;
         }
 