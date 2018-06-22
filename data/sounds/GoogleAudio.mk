bd521665c7c11a9edd91195fe3afb037ac4713ad
RakeshBatra <raakesh.batra@rediffmail.com>
2018-06-22 00:34:14 +0000

    Update Sounds

--- data/sounds/GoogleAudio.mk
+++ data/sounds/GoogleAudio.mk
@@ -39,14 +39,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)material/effects/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
     $(LOCAL_PATH)material/effects/camera_focus.ogg:system/media/audio/ui/camera_focus.ogg \
     $(LOCAL_PATH)material/effects/Dock.ogg:system/media/audio/ui/Dock.ogg \
-    $(LOCAL_PATH)material/effects/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
+    $(LOCAL_PATH)effects/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
     $(LOCAL_PATH)material/effects/GoodBattery.ogg:system/media/audio/ui/GoodBattery.ogg \
     $(LOCAL_PATH)material/effects/KeypressDelete.ogg:system/media/audio/ui/KeypressDelete.ogg \
     $(LOCAL_PATH)material/effects/KeypressInvalid.ogg:system/media/audio/ui/KeypressInvalid.ogg \
     $(LOCAL_PATH)material/effects/KeypressReturn.ogg:system/media/audio/ui/KeypressReturn.ogg \
     $(LOCAL_PATH)material/effects/KeypressSpacebar.ogg:system/media/audio/ui/KeypressSpacebar.ogg \
     $(LOCAL_PATH)material/effects/KeypressStandard.ogg:system/media/audio/ui/KeypressStandard.ogg \
-    $(LOCAL_PATH)material/effects/Lock.ogg:system/media/audio/ui/Lock.ogg \
+    $(LOCAL_PATH)effects/Lock.ogg:system/media/audio/ui/Lock.ogg \
     $(LOCAL_PATH)material/effects/LowBattery.ogg:system/media/audio/ui/LowBattery.ogg \
     $(LOCAL_PATH)material/effects/NFCFailure.ogg:system/media/audio/ui/NFCFailure.ogg \
     $(LOCAL_PATH)material/effects/NFCInitiated.ogg:system/media/audio/ui/NFCInitiated.ogg \
@@ -55,7 +55,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)material/effects/NFCTransferInitiated.ogg:system/media/audio/ui/NFCTransferInitiated.ogg \
     $(LOCAL_PATH)material/effects/Trusted.ogg:system/media/audio/ui/Trusted.ogg \
     $(LOCAL_PATH)material/effects/Undock.ogg:system/media/audio/ui/Undock.ogg \
-    $(LOCAL_PATH)material/effects/Unlock.ogg:system/media/audio/ui/Unlock.ogg \
+    $(LOCAL_PATH)effects/Unlock.ogg:system/media/audio/ui/Unlock.ogg \
     $(LOCAL_PATH)material/effects/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
     $(LOCAL_PATH)material/effects/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg \
     $(LOCAL_PATH)material/effects/WirelessChargingStarted.ogg:system/media/audio/ui/WirelessChargingStarted.ogg \
